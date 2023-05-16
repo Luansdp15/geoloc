489424ff9bff34e19044a7c4cf0384e45ee1952f
Luansdp15 <luan.pereira66@etec.sp.gov.br>
2023-05-17 02:24:27 +0900

    Project's Initial Commit

---
+++ www/components/script.js
@@ -0,0 +1,32 @@
+ $(document).on("click","#geo",function()
+         {
+
+             var onSucess = function(position) {
+                 alert('Latitude: '     +position.coords.latitude   +'\n'+
+'Longitude: '     +position.coords.longitude   +'\n'+
+
+'Altitude: '     +position.coords.altitude   +'\n'+
+
+'Accuracy: '     +position.coords.accuracy   +'\n'+
+
+'Altitude Accuracy: '     +position.coords.altitudeAccuracy   +'\n'+
+
+'Heading: '     +position.coords.heading   +'\n'+
+
+'Speed: '     +position.coords.speed   +'\n'+
+
+'Timestamp: '     +position.coords.timestamp   +'\n');
+
+             };
+
+             //onError Callback receives a Position object
+             //
+
+             function onError(error) {
+                 alert('code:' +error.code + '\n' +
+                 'message: ' + error.message + '\n');
+             }
+
+             navigator.geolocation.getCurrentPosition(onSucess, onError);
+
+         });
